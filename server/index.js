fafe06ddfab500d4b6b8d614db2664ac7b4ad900
Aryan Mehta <aryanmht9@gmail.com>
2025-03-13 15:05:18 -0600

    removed 5000 port

--- server/index.js
+++ server/index.js
@@ -8,7 +8,4 @@ app.get("/", (req, res) => {
     res.send("Server is Running!");
 });
 
-const PORT = 5000; // Choose any available port
-app.listen(PORT, () => {
-    console.log(`🚀 Server Running on http://localhost:${PORT}`);
-});
+app.listen();
